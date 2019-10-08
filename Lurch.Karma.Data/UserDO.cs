151caf917c3754b4e4d01d005c035c6b5d5bb0d4
Yacine Lakel <lakel.1709@gmail.com>
2019-10-08 11:20:09 +0200

    init work

---
+++ Lurch.Karma.Data/UserDO.cs
@@ -0,0 +1,25 @@
+﻿using Lurch.Karma.Core;
+
+namespace Lurch.Karma.Data
+{
+    public class UserDo
+    {
+        public int Id { get; set; }
+
+        public int KarmaAmount { get; set; }
+
+        public static User ToModel(UserDo obj)
+        {
+            return new User(obj.Id, obj.KarmaAmount);
+        }
+
+        public static UserDo ToDo(User user)
+        {
+            return new UserDo
+            {
+                Id = user.Id,
+                KarmaAmount = user.Karma.Amount
+            };
+        }
+    }
+}
